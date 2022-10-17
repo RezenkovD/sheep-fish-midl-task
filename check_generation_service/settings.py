c6962110bbb9847144e156c99364477dbaeaf8ef
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-17 13:21:32 +0300

    Connect to db

--- check_generation_service/settings.py
+++ check_generation_service/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'check_generation_service.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
