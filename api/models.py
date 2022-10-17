1040ca53f45a384a1d53f8e0e1cb3c202b055576
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-17 14:40:00 +0300

    Create model 'Printer'

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Printer(models.Model):
+    name = models.CharField(max_length=64, help_text="Printer name")
+    api_key = models.CharField(max_length=128, help_text="API access key")
+    KITCHEN = "KT"
+    CLIENT = "CL"
+    CHOICES = [
+        (KITCHEN, "Kitchen"),
+        (CLIENT, "Client"),
+    ]
+    check_type = models.CharField(
+        max_length=2, choices=CHOICES, default=KITCHEN, help_text="The type of receipt printed by the printer"
+    )
+    point_id = models.IntegerField(help_text="The point to which the printer is bound")
