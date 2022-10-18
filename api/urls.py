313502eb0af648ba1f1a16898380f4d626a9daec
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-18 09:26:59 +0300

    Create endpoint for models Print and Check

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,10 @@ from .import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+router.register(r"printers", views.PrintViewSet)
+router.register(r"checks", views.CheckViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.serializers import PrinterSerializer, CheckSerializer
+from api.models import Printer, Check
 
-# Create your views here.
+
+class PrintViewSet(viewsets.ModelViewSet):
+    queryset = Printer.objects.all()
+    serializer_class = PrinterSerializer
+
+
+class CheckViewSet(viewsets.ModelViewSet):
+    queryset = Check.objects.all()
+    serializer_class = CheckSerializer
